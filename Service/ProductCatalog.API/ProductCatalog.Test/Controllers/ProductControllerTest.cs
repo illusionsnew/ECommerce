1dd224e3a93b393d286b703ca563d4c7aa5b4e98
sajithgs <sajithgs@gmail.com>
2017-03-06 21:05:44 -0600

    Created Test Project

---
+++ Service/ProductCatalog.API/ProductCatalog.Test/Controllers/ProductControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProductCatalog.Test.Controllers
+{
+    public class ProductControllerTest
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
