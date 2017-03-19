ecaceb79a38e70f6a6663f3cbc64d5aa49c8bf56
sajithgs <sajithgs@gmail.com>
2017-03-18 20:34:41 -0500

    Integrated Web-API with SQL

---
+++ Service/ProductCatalog.API/ProductCatalog.API/Services/Catalog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductCatalog.DA;
+
+namespace ProductCatalog.API.Services
+{
+    public class Catalog : ICatalog
+    {
+        private CatalogDB catalogDB;
+
+        public Catalog()
+        {
+            catalogDB = new CatalogDB();
+        }
+        public Product FindProductById(int productId)
+        {
+            return catalogDB.Products.FirstOrDefault(p => p.Id == productId);
+        }
+
+        public Product FindProductByName(string productName)
+        {
+            return catalogDB.Products.FirstOrDefault(p => p.Name == productName);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return catalogDB.Products;
+        }
+    }
+}
---
+++ Service/ProductCatalog.API/ProductCatalog.API/Services/ICatalog.cs
@@ -0,0 +1,15 @@
+﻿using ProductCatalog.DA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.API.Services
+{
+    public interface ICatalog
+    {
+        IEnumerable<Product> GetProducts();
+        Product FindProductById(int productId);
+        Product FindProductByName(string productName);
+    }
+}
